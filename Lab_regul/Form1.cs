66297f80a1cc126883ed3dc698e201fc13fd1649
TreshUp <Timka500@inbox.ru>
2016-12-22 04:35:47 +0600

    Almost done

--- Lab_regul/Form1.cs
+++ Lab_regul/Form1.cs
@@ -18,7 +18,7 @@ namespace Lab_regul
         }
         Data data = new Data();
         System.Text.RegularExpressions.Match m;
-        string[] text_result;
+        Dictionary<string, int> freq = new Dictionary<string,int>();
         private void Open_File(object sender, EventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
@@ -27,14 +27,9 @@ namespace Lab_regul
             DialogResult res = dlg.ShowDialog();
             if (res == DialogResult.OK)
             {
-                //Stopwatch sw = new Stopwatch(); sw.Start(); // using System.Diagnostics
                 data.fileName = dlg.FileName;
                 data.ReadFromFile();
                 richTextBox1.Text = data.txt;
-                data.SplitText(text_result);
-                //data.ReadFromFile();
-                //sw.Stop();
-                //Console.WriteLine("{0}ms reading file", sw.ElapsedMilliseconds);
 
             }
 
@@ -42,16 +37,70 @@ namespace Lab_regul
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < ; i++)
+            for (int i = 1; i < 2;/*i < data.result.Length;*/ i++)
             {
-                Find();
+                Find(data.result[i]);
+                //freq = freq.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+                foreach (var p in freq)
+                {
+                    Console.Write(p.Key);
+                    Console.Write("-");
+                    Console.WriteLine(p.Value);
+                }
+                //var max = freq.Select(k => freq.Max());
+                var first = freq.Select(k => freq.First());
+                
             }
             
         }
-
+        private void Add(string poisk)
+        {
+            int n = 0;
+            //if (freq[m.ToString()] != 0 && !freq[m.ToString()])
+            if (freq.ContainsKey(m.ToString()))
+            {
+                n = freq[m.ToString()];
+                freq.Remove(m.ToString());
+                freq.Add(m.ToString(), n + 1);
+            }
+            else
+            {
+                freq.Add(m.ToString(), 1);
+            }
+        }
         private void Find(string result)
         {
             m = data.Find(result);
+            Add(m.ToString());
+            //if (freq[m.ToString()] != 0)
+            //{
+            //    n = freq[m.ToString()];
+            //    freq.Remove(m.ToString());
+            //    freq.Add(m.ToString(), n + 1);
+            //}
+            //else
+            //{
+            //    freq.Add(m.ToString(), 1);
+            //}
+            //foreach (var p in freq)
+            //{
+            //    if (m.ToString() == p.Key) p.Value = p.Value + 1;
+            //}
+            //Console.Write(m);
+            //Console.Write("-----");
+            while(true)
+            {
+                if (m != null && m.Success)
+                {
+                    m = m.NextMatch();
+                    Add(m.ToString());
+                    //Console.Write(m);
+                    //Console.Write("-----");
+                    
+                }
+                else break;
+                
+            }
         }
     }
 }
