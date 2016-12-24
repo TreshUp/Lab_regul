9ab2ed9198d5ce154ec9aa473b9ef31510e9ca17
TreshUp <Timka500@inbox.ru>
2016-12-24 21:01:57 +0600

    Data

--- Lab_regul/Data.cs
+++ Lab_regul/Data.cs
@@ -8,27 +8,35 @@ using System.IO;
 
 namespace Lab_regul
 {
-    class Data
+    class Data 
     {
         internal string txt = ""; // текст для обработки
         internal string fileName; // имя открытого файла  
-        internal string pattern;
+        static internal string pattern="[LMXIV]+\n";    
+        internal string[] result;
         internal void ReadFromFile()
         {
             using (StreamReader sr = new StreamReader(fileName))
             {
                 txt = sr.ReadToEnd().Replace("\r\n", "\n");
             }
-            
-            //foreach (Match match in Regex.Matches(txt, @"([LMXIV]+)\n[^<]+?([LMXIV]+)\n", RegexOptions.IgnoreCase))
-            //    Console.WriteLine(match);
             pattern = "[LMXIV]+\n";
-            string[] result = Regex.Split(txt, pattern,
-                                    RegexOptions.IgnoreCase);
-            for (int i = 1; i < 4; i++)
-            {
-                Console.WriteLine(result[i]);
-            }
+            result = Regex.Split(txt, pattern, RegexOptions.IgnoreCase);
+            //for (int i = 1; i < result.Length; i++)
+            //{
+            //    Console.WriteLine(result[i]);
+            //}
+        }
+        //internal void SplitText(string[] text_result)
+        //{
+        //    text_result = Regex.Split(txt, pattern, RegexOptions.IgnoreCase);
+        //}
+        internal Match Find(string result)
+        {
+            //Regex r = new Regex(@"(\b)(\w)(\w)(\w)(\w)\b");
+            Regex r = new Regex(@"(\b)(\w){4}\b");
+            //Regex r = new Regex(@"(\b[a-z]{4}\b)*(\b[A-Z]{3}\b)");
+            return r.Match(result);
         }
     }
 }
